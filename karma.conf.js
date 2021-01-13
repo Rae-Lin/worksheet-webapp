05384d60944e946dc17c4b1109035a4d32169c69
josh.chen <josh.chen@ptc-nec.com.tw>
2021-01-13 19:17:10 +0800

    調整專案名稱

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/CalendarTest'),
+      dir: require('path').join(__dirname, './coverage/WorkSheet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
